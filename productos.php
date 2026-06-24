a7ccbcbc35443aaf407052aaa2ef629cedc248d0
my22002 <my22002@ues.edu.sv>
2026-06-24 13:47:45 -0600

    otro div

--- productos.php
+++ productos.php
@@ -56,13 +56,17 @@ $productos = mysqli_query($conn, "SELECT * FROM producto ORDER BY id DESC");
 <body>
     <div class="navbar-custom">
         <span class="navbar-title">ERP Auto Repuestos</span>
+        <div class="navbar-custom">
+        <span class="navbar-title">ERP Auto Repuestos</span>
         <div>
             <a href="index.php">Inicio</a>
             <a href="productos.php">Productos</a>
+            <a href="clientes.php">Clientes</a>
             <a href="ventas.php">Ventas</a>
             <a href="compras.php">Compras</a>
             <a href="librodiario.php">Libro Diario</a>
             <a href="catalogo.php">Catálogo y Manual</a>
+            <a href="reportes.php">Reportes</a>
         </div>
     </div>
 
